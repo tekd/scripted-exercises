ac63d6d6e3ce0d420c6d991148fa1976a48fb234
Denny Tek <tek.denny@gmail.com>
2015-11-19 16:49:25 -0500

    Revise JS exercises

--- javascript-exercises.js
+++ javascript-exercises.js
@@ -1,37 +1,37 @@
-//1. Create a function that gives me the area of a square.
+// 1. Create a function that gives me the area of a rectangle.
 
-//TEST1
-console.log(findArea(3,3) === 9);
+// TEST1
+// console.log(findArea(4,3) === 12);
 
-//2. Create a function that gives me the perimeter of a square.
+// 2. Create a function that gives me the perimeter of a rectangle.
 
-//TEST2
-console.log(findPerimeter(4,3) === 14);
+// TEST2
+// console.log(findPerimeter(4,3) === 14);
 
-//3. Create a function that converts a temperature in Celsius to Fahrenheit.
+// 3. Create a function that converts a temperature in Celsius to Fahrenheit.
 
-//TEST3
-console.log(convertCelsiusToFarenheit(34) === 93.2);
+// TEST3
+// console.log(convertCelsiusToFarenheit(34) === 93.2);
 
-//4. Create a function that converts a temperature in Fahrenheit to Celsius.
+// 4. Create a function that converts a temperature in Fahrenheit to Celsius.
 
-//TEST4
-console.log(convertFarenheitToCelsius(212)===100);
+// TEST4
+// console.log(convertFarenheitToCelsius(212)===100);
 
-//5. Create a function that converts ounces to cups
+// 5. Create a function that converts ounces to cups.
 
-//TEST5
-console.log(convertOuncesToCups(28)=== 3.5);
+// TEST5
+// console.log(convertOuncesToCups(28)=== 3.5);
 
-//6. Create a function that takes a name as a parameter and prints a greeting with the name.
+// 6. Create a function that takes a name as a parameter and prints a greeting with the name.
 
-//7. Create a function that takes in a string and returns the number of characters in the string.
+// 7. Create a function that takes in a string and returns the number of characters in the string.
 
-//8. Create a function that takes in a string and then YELLS IT BACK IN UPPERCASE LETTERS
+// 8. Create a function that takes in a string and then YELLS IT BACK IN UPPERCASE LETTERS
 
-//9. Create a function that simulates a 12-sided die.
+// 9. Create a function that simulates a 12-sided die.
 
-//10. Create a Pythagorean function in JavaScript.
+// 10. Create a Pythagorean function in JavaScript.
 
-//11. Create a function that will take a string and return it with all lowercase characters.
+// 11. Create a function that will take a string and return it with all lowercase characters.
 
